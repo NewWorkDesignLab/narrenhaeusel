c6cfeda02b93ab6fc010eafaa2959e55df9cb97b
Jonas <116257690+j0nes-L@users.noreply.github.com>
2025-12-21 22:21:34 +0100

    Revert "Replace A-Frame AR with model-viewer for AR experience"
    This reverts commit ebfb2cce816a48fea2ae3836739b50d03b1a22b5.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://newworkdesignlab.github.io',
-  base: '/narrenhaeusel',
-  server: {
-    host: true,
-    port: 4321
-  }
+  base: '/narrenhaeusel', 
 });
